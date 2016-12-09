d9b17ff6078242ad9ccae11aa29be9cb9e551073
Alexander Osin <alexandre.osin@gmail.com>
2016-12-09 22:18:06 +0200

    db configuration files

--- Server/db_downgrade.py
+++ Server/db_downgrade.py
@@ -5,3 +5,4 @@ v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v-1)
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print('Current database version: ' + str(v))
+
