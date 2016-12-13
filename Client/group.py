c9f6fef01fdab03ed263d4c026e1fe4c25323660
Alexander Osin <alexandre.osin@gmail.com>
2016-12-13 02:20:24 +0200

    Created main window.

---
+++ Client/group.py
@@ -0,0 +1,39 @@
+from tkinter import *
+import tkinter.messagebox as messagebox
+import requests
+from json import loads
+from widgets.lists import ScrolledList
+from widgets.lists import GroupScrolledList
+from widgets.lists import StudentScrolledList
+from widgets.quitter import Quitter
+
+def get_students(event, text, studentList):
+    r = requests.get('http://localhost:5000/query/' + str(text))
+    receive = loads(r.text)
+    studentList.fillListbox(receive['persons'])
+
+def main():
+    root = Tk()
+    root.title('StudentGroupApp')
+    root.minsize(10, 10)
+    root.maxsize(100, 10)
+    root.maxsize
+    try:
+        r = requests.get('http://localhost:5000/groups')
+        j = loads(r.text)
+        options = [x['code'] for x in j['groups']]
+        v = StringVar()
+        ent = Entry(root, textvariable=v)
+        ent.pack(side=TOP, expand=YES, fill=X)
+        ent.insert(0, 'Enter here...')
+        Quitter().pack(side=BOTTOM, anchor=E)
+        st = StudentScrolledList(parent=root, key=('name', 'surname'), title='Students')
+        st.pack(side=RIGHT)
+        GroupScrolledList(st, elements=j['groups'], parent=root, key=('code',), title='Groups').pack()
+        ent.bind('<Shift-Up>', lambda i=ent, x=v.get(), y=st: get_students(i, v.get(), y))
+    except Exception as e: 
+        messagebox.showerror(message=e)#'Cannot connect to server.')
+    root.mainloop() 
+
+if __name__=='__main__':
+    main()
